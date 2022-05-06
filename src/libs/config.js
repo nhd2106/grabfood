43f3cb756ffde5e2a80c2df76862b8926e79f43e
Lê Trọng Hiếu <38271331+github76134@users.noreply.github.com>
2022-05-06 17:12:52 +0700

    Add files via upload

---
+++ src/libs/config.js
@@ -0,0 +1,10 @@
+export const URL_DOMAIN = "http://localhost:3000/menu/";
+export const TITLE_MENU = "thuc-don-xbot"
+export const NAME_TITLE = "Thực đơn"
+export const URL_BACKEND = "http://localhost:1235/users";
+// export const URL_BACKEND = "http://serverlth.ddns.net:1235";
+export const URL_IMG_FOOD = "https://lavenderstudio.com.vn/wp-content/uploads/2017/03/chup-san-pham.jpg";
+
+
+
+export const TITLE_TOOLTIP_PRICE = "Xin lưu ý rằng mức giá này chỉ mang tính chất tham khảo. Nó có thể thay đổi do phí giao hàng hoặc mã giảm giá. Nếu anh/chị được mời thì không cần quan tâm giá này."
